1789cce1e2ed066e0491d1dabe4a577e1c3b14ea
AkhilParim <akhil.parim@gmail.com>
2025-09-19 10:57:48 -0400

    Dev and Prod configuration files

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,7 +1,10 @@
 declare const process: any;
 
 export const environment = {
+    production: true,
     mapboxToken: process.env.MAPBOX_TOKEN || '',
-    apiUrl: 'http://localhost:3050',
+    apiUrl: 'http://52.15.77.33/placeEditor',
     FACILITATOR_ZIP_PASSWORD: process.env.FACILITATOR_ZIP_PASSWORD || ''
 };
+
+
