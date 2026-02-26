f6567191a2ca679531dfb92adeb01c29792a3108
AkhilParim <akhil.parim@gmail.com>
2026-02-26 17:07:18 -0500

    Update env

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -3,7 +3,7 @@ declare const process: any;
 export const environment = {
     production: true,
     mapboxToken: process.env.MAPBOX_TOKEN || '',
-    apiUrl: 'http://52.15.77.33/placeEditor',
+    apiUrl: 'http://<your-server-url>/placeEditor',
     FACILITATOR_ZIP_PASSWORD: process.env.FACILITATOR_ZIP_PASSWORD || ''
 };
 
